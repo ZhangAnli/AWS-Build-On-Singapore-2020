6c0be6ed0cd5d75d9e737b19926570d8637547a2
peirong.wu <wupeirong294@gmail.com>
2020-08-27 19:51:41 +0800

    Add risk score hover tooltip

--- aws-build-on-singapore-2020/src/Components/Data/Data.jsx
+++ aws-build-on-singapore-2020/src/Components/Data/Data.jsx
@@ -7,6 +7,7 @@ import Paper from '@material-ui/core/Paper';
   export default class App extends Component {
     state = {
       ageData: [],
+      riskData: [],
       barState: {
         labels: ['Cluster 1', 'Cluster 2', 'Cluster 3 ',
                'Cluster 4', 'Cluster 5', 'Cluster 6', 'Cluster 7'],
@@ -24,12 +25,14 @@ import Paper from '@material-ui/core/Paper';
 
     componentDidMount() {
       axios.get(`https://cors-anywhere.herokuapp.com/https://ebjyty6n50.execute-api.us-east-2.amazonaws.com/awsbuildon/get`)
-      .then(res => {        
+      .then(res => {             
         const numData = res.data[0];
-        const ageData = res.data[1];        
+        const ageData = res.data[1];                
+        const riskData = res.data[2];                
         this.setState(
           {
             ageData: ageData,
+            riskData: riskData,
             barState: {
               labels: [...this.state.barState.labels],
               datasets: [{...this.state.barState.datasets[0], data: numData}]
@@ -56,10 +59,12 @@ import Paper from '@material-ui/core/Paper';
               },
               tooltips: {
                 callbacks: {
-                  label: (tooltipItem, data) => {
-                    let originalLabel = `Number of Clients: ${tooltipItem.value}`;
-                    const ageLabel = `Median Age: ${this.state.ageData[tooltipItem.datasetIndex]}`;                    
-                    return [originalLabel, ageLabel];
+                  label: (tooltipItem, data) => {                    
+                    const idx = tooltipItem.index;
+                    const originalLabel = `Number of Clients: ${tooltipItem.value}`;
+                    const ageLabel = `Median Age: ${this.state.ageData[idx]}`;
+                    const risklabel = `Median Risk Level (Out of 10): ${this.state.riskData[idx]}`;                                       
+                    return [originalLabel, ageLabel, risklabel];
                   }                  
                 }
               }
