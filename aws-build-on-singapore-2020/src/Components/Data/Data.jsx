1205a8b304f1284e9b0dac8844d36664976d949b
peirong.wu <wupeirong294@gmail.com>
2020-08-27 19:11:27 +0800

    Refactor state for extra data

--- aws-build-on-singapore-2020/src/Components/Data/Data.jsx
+++ aws-build-on-singapore-2020/src/Components/Data/Data.jsx
@@ -6,32 +6,41 @@ import Paper from '@material-ui/core/Paper';
     
   export default class App extends Component {
     state = {
-      labels: ['Cluster 1', 'Cluster 2', 'Cluster 3 ',
+      barState: {
+        labels: ['Cluster 1', 'Cluster 2', 'Cluster 3 ',
                'Cluster 4', 'Cluster 5', 'Cluster 6', 'Cluster 7'],
-      datasets: [
-        {
-          label: 'Client Count',
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgba(0,0,0,0)',
-          borderWidth: 2,
-          data: [],
-        }
-      ]
+        datasets: [
+          {
+            label: 'Client Count',
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,0)',
+            borderWidth: 2,
+            data: [],
+          }
+        ]
+      }      
     }
 
     componentDidMount() {
       axios.get(`https://cors-anywhere.herokuapp.com/https://ebjyty6n50.execute-api.us-east-2.amazonaws.com/awsbuildon/get`)
-      .then(res => {
-        const data = res.data;
-        this.setState({datasets: [{...this.state.datasets[0], data}]});
-      })
+      .then(res => {        
+        const data = res.data[0];        
+        this.setState(
+          {
+            barState: {
+              labels: [...this.state.barState.labels],
+              datasets: [{...this.state.barState.datasets[0], data}]
+            }
+          }
+        );        
+      }).catch(err => console.log(err));
     }
 
     render() {
       return (
         <Paper className="paper" elevation={10}>
           <Bar
-            data={this.state}
+            data={this.state.barState}
             options={{
               title:{
                 display:true,
